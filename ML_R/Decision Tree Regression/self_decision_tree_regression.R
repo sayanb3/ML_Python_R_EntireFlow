154dcd00c62351a2cc482228e951ab1afaa3d53e
sayanb3 <sayanb3@gmail.com>
2021-07-27 17:59:32 -1200

    Add files via upload

---
+++ ML_R/Decision Tree Regression/self_decision_tree_regression.R
@@ -0,0 +1,42 @@
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]#indexes of columns we want to keep. last 2 columns
+
+# Splitting the dataset into the Training set and Test set
+# # install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling not needed. Since its nothing but a MLR model with polynomial
+#terms. 
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+
+# Fitting Decision Tree Regression to the dataset.
+#install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+#create your regressor here
+
+# Predicting a new result 
+predict(regressor, data.frame(Level = 6.5))
+
+# Visualizing the  Regression Model results for higher resolution and smoother curve
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level) , 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level=x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Decision Tree Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
+
